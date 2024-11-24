568dfd081006180ee5f51751a9b9dd2d87022b95
Aqdas Ali <aqdasali584@gmail.com>
2024-11-24 23:42:58 +0500

    Obj To Primitive Con

--- Session4/D_ObjToPrimitiveCon.js
+++ Session4/D_ObjToPrimitiveCon.js
@@ -8,10 +8,13 @@ obj.toString = function() {
     return "Object as string";
 };
 console.log(obj.toString()); // "Object as string"
+console.log(typeof obj.toString());//string
+
 
 
 // Implicit Conversion:
 // During string concatenation:
-
-let obj = { value: 10 };
-console.log("Value is: " + obj); // "Value is: [object Object]"
+let obj1 = { value: 10 };
+// console.log("Value is: " + obj1.toString()); // "Value is: [object Object]" (Ider ma na explicitly is ko str banaya ha jab ka is ka ilawa implicitly bi yea kam ho jata ha jaisy)
+console.log("Value is: " + obj1) // "Value is: [object Object]"
+console.log(typeof obj1);//object
