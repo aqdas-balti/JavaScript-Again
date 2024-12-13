f31527958e568d0aa1f3500c8f64487271a091b3
Aqdas Ali <aqdasali584@gmail.com>
2024-12-13 14:28:39 +0500

    Squential-Flow

--- Session8/Sequentila-Control-Flow/Sequential.js
+++ Session8/Sequentila-Control-Flow/Sequential.js
@@ -11,10 +11,8 @@ console.log("Step 3"); // Finally, this will execute third
 Explanation:
 console.log("Step 1");
 Pehla line chal kar screen par "Step 1" print karega.
-
 console.log("Step 2");
 Iske baad dusra line chal kar "Step 2" print karega.
-
 console.log("Step 3");
 Akhri line chal kar "Step 3" print karega.
 */
