de309d4d0bbc161c83bcc590f8aae5dfacaf1d40
Aqdas Ali <aqdasali584@gmail.com>
2024-12-13 15:57:35 +0500

    For-Loop

---
+++ Session9/ForLoop.js
@@ -0,0 +1,26 @@
+/*
+1. for Loop
+The for loop repeats a block of code a specific number of times. It has three parts:
+
+Initialization: Set the starting value (e.g., let i = 0).
+Condition: Check if the loop should continue (e.g., i < 5).
+Increment/Decrement: Update the value after each iteration (e.g., i++).
+Example:*/
+
+for (let i = 0; i < 5; i++) {
+    console.log("Count:", i);
+}
+
+/*
+Explanation:
+
+for (let i = 0; i < 5; i++) {
+Yahan loop start hota hai. i = 0 se shuru karega, jab tak i < 5 hai tab tak chalega, aur har baar i++ (1 increase) karega.
+
+console.log("Count:", i);
+Har iteration par i ki value print karega (0 se 4 tak).
+*/
+let name = "Aqdas";
+for (let i = 0; i < name.length; i++) {
+    console.log(name[i]);
+}
