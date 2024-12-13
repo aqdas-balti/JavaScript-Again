0824c64478a13fa3d0396d20cd517fd8c8b51c20
Aqdas Ali <aqdasali584@gmail.com>
2024-12-13 14:28:01 +0500

    Switch

---
+++ Session8/Sequentila-Control-Flow/Sequential.js
@@ -0,0 +1,20 @@
+/*
+1. Sequential (Default Mode)
+Introduction:
+Sequential execution means the code runs line by line from top to bottom unless a special statement (like a condition or loop) changes the flow.
+
+Example:*/
+console.log("Step 1"); // This will execute first
+console.log("Step 2"); // Then this will execute second
+console.log("Step 3"); // Finally, this will execute third
+/*
+Explanation:
+console.log("Step 1");
+Pehla line chal kar screen par "Step 1" print karega.
+
+console.log("Step 2");
+Iske baad dusra line chal kar "Step 2" print karega.
+
+console.log("Step 3");
+Akhri line chal kar "Step 3" print karega.
+*/
