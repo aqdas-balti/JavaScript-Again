e47a5e98d09bc371fcca2470d6186b2f37ef82b8
Aqdas Ali <aqdasali584@gmail.com>
2024-12-13 19:00:03 +0500

    WhileLoop

--- Session9/WhileLoop.js
+++ Session9/WhileLoop.js
@@ -4,10 +4,10 @@ The while loop executes code as long as the condition is true. Use it when the n
 
 Example:*/
 
-let i = 0;
+let a = 0;
 while (i < 5) {
-    console.log("Count:", i);
-    i++;
+    console.log("Count:", a);
+    a++;
 }
 
 /*
@@ -26,3 +26,20 @@ while (j < name.length) {
     j++;
 }
 //In this example, the while loop iterates over the characters of the string "Aqdas" and prints each character.
+
+let prompt2 = require('prompt-sync')();
+console.log("While Loop");
+let val = prompt2("Enter a number: ");
+let i = 1;
+while (i <= 5) {
+    console.log(val, "*", i, "=", val * i);
+    i++;
+}
+//Explanation:
+/*
+let val = prompt2("Enter a number: ");
+Yahan loop start hota hai. i = 1 se shuru karega, jab tak i <= 10 hai tab tak chalega, aur har baar i++ (1 increase) karega.
+console.log(val, "*", i, "=", val * i);
+Har iteration par i ki value print karega (1 se 10 tak).
+*/
+
