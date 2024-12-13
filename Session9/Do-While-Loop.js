d7bf2bf10069111ae73a3775c26112e65530e449
Aqdas Ali <aqdasali584@gmail.com>
2024-12-13 19:00:48 +0500

    do-WhileLoop

--- Session9/Do-While-Loop.js
+++ Session9/Do-While-Loop.js
@@ -18,9 +18,25 @@ console.log("Count:", i);
 Har iteration par i ki value print karega (0 se 4 tak).
 */
 let name = "Aqdas";
-let j = 0;
+let a = 0;
 do {
-    console.log(name[j]);
-    j++;
+    console.log(name[a]);
+    a++;
 } while (j < name.length);
 //In this example, the do...while loop iterates over the characters of the string "Aqdas" and prints each character.
+
+let prompt3 = require('prompt-sync')();
+console.log("Do-While Loop");
+let val2 = prompt3("Enter a number: ");
+let j = 1;
+do {
+    console.log(val2, "*", j, "=", val2 * j);
+    j++;
+} while (j <= 5);
+//Explanation:
+/*
+let val2 = prompt3("Enter a number: ");
+Yahan loop start hota hai. i = 1 se shuru karega, jab tak i <= 10 hai tab tak chalega, aur har baar i++ (1 increase) karega.
+console.log(val2, "*", j, "=", val2 * j);
+Har iteration par i ki value print karega (1 se 10 tak).
+*/
